4ffd7c957b8e6778d332dd498b54a143a3d134b4
Emrah Kostem <kostem@gmail.com>
2013-03-07 15:10:34 -0800

    First commit

---
+++ GETUP.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+## INIT
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -Sso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
+## WHERE THINGS ARE
+cd ~/.vim/bundle
+
+## AN UGLY WAY TO GET & KEEP BUNDLES UP TO DATE
+git clone https://github.com/Rip-Rip/clang_complete.git
+cd clang_complete
+git pull
+cd ..
+
+git clone https://github.com/tomasr/molokai
+cd molokai
+git pull
+cd ..
+
+git clone https://github.com/scrooloose/nerdtree.git
+cd nerdtree
+git pull
+cd ..
+
+git clone https://github.com/ervandew/supertab.git
+cd supertab
+git pull
+cd ..
+
+git clone https://github.com/scrooloose/nerdcommenter.git
+cd nerdcommenter
+git pull
+cd ..
+
+git clone https://github.com/tsaleh/vim-matchit.git
+cd vim-matchit
+git pull
+cd ..
+
+git clone https://github.com/widox/vim-buffer-explorer-plugin.git
+cd vim-buffer-explorer-plugin
+git pull
+cd ..
+
+git clone https://github.com/AlphaLiu/a.vim.git
+cd a.vim
+git pull
+cd ..
+
+git clone https://github.com/vim-scripts/taglist.vim.git
+cd taglist.vim
+git pull
+cd ..
+
+## REPEAT FOR NEW BUNDLES
+
---
+++ README.txt
@@ -0,0 +1,7 @@
+Use the script GETUP.sh to clone and update bundles.
+Script creates the directories under ~/.vim
+
+Copy the vimrc_template file as .vimrc under your home directory.
+Using as many comments as possible is very much appreciated.
+
+ENJOY!
